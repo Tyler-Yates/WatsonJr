99547d8a526b68e4c3c3d0a8d9f0058a04a18aff
Tyler Yates <tyler.yates@utexas.edu>
2016-05-17 19:49:58 -0500

    Add clarification to LuceneRanker Javadoc.

--- src/main/java/com/aqa/candidates/LuceneRanker.java
+++ src/main/java/com/aqa/candidates/LuceneRanker.java
@@ -1,10 +1,15 @@
 package com.aqa.candidates;
 
+import com.aqa.kb.Document;
 import com.aqa.kb.KnowledgeBase;
 
 /**
  * Class that uses the Lucene search engine to answer questions. You will need to implement the {@link
  * #answerQuestion(KnowledgeBase, String)} method.
+ * <p>
+ * You should only access the text of the documents (using {@link Document#getText()}) from the knowledge base. You will
+ * not need to use the information from CoreNLP in the document because Lucene will handle the natural language
+ * processing for you.
  */
 public class LuceneRanker implements Ranker {
     @Override
