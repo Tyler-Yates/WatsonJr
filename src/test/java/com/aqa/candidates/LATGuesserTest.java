dd6283d3d2dfaff9d84ab9fb9dc82c576d3ce420
Tyler Yates <tyler.yates@utexas.edu>
2016-06-08 23:26:18 +0100

    Break up LATGuesserTest into more test cases.

--- src/test/java/com/aqa/candidates/LATGuesserTest.java
+++ src/test/java/com/aqa/candidates/LATGuesserTest.java
@@ -7,19 +7,29 @@ import static org.junit.Assert.assertEquals;
 
 public class LATGuesserTest {
     @Test
-    public void guessLAT() throws Exception {
+    public void guessLATPerson() throws Exception {
         // PERSON questions
         assertEquals(LAT.PERSON, guess("Who was the first king of Portugal?"));
         assertEquals(LAT.PERSON, guess("Who is the prime minister of England?"));
+    }
 
+    @Test
+    public void guessLATLocation() throws Exception {
         // LOCATION questions
         assertEquals(LAT.LOCATION, guess("Where is the highest point in Portugal?"));
         assertEquals(LAT.LOCATION, guess("Where did Columbus sail to?"));
+    }
 
+    @Test
+    public void guessLATFood() throws Exception {
         // FOOD questions
         assertEquals(LAT.FOOD, guess("What 'royal' cake is eaten during Natal in Portugal?"));
         assertEquals(LAT.FOOD, guess("What tasty treat comes from an eight-legged creature?"));
+    }
 
+
+    @Test
+    public void guessLATGame() throws Exception {
         // GAME questions
         assertEquals(LAT.GAME, guess("What sport is the most popular worldwide?"));
         assertEquals(LAT.GAME, guess("What is played using a puck and ice-skates?"));
